be0074d3dd85357a9acf8f3006d0e5b656abcee1
Алексей Фомин <afomin@fil-it.ru>
2022-04-14 17:55:40 +0300

    cut-version created

--- src/main/java/com/lesson/springdatajpa/service/exception/room/RoomException.java
+++
@@ -1,7 +0,0 @@
-package com.lesson.springdatajpa.service.exception.room;
-
-public class RoomException extends RuntimeException {
-    public RoomException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/lesson/springdatajpa/service/exception/room/RoomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lesson.springdatajpa.service.exception.room;
-
-public class RoomNotFoundException extends RoomException{
-    public RoomNotFoundException(String id) {
-        super("There is no room with id " + id);
-    }
-}
